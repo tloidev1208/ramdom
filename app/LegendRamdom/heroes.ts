399da65d3bbb7aa20837cf49984962dd471c30dc
tloidev1208 <thanh.loi@bs.io.vn>
2025-08-20 09:32:35 +0700

    update icon for role

--- app/LegendRamdom/heroes.ts
+++ app/LegendRamdom/heroes.ts
@@ -1,32 +1,32 @@
 // heroes.ts
 export const heroes = {
-  "Đấu sĩ": [
+  "🗡️Đấu sĩ": [
     "Taara", "Triệu Vân", "Butterfly", "Volkath", "Arduin", "Florentino", "Zuka",
     "Maloch", "Zephys", "Lữ Bố", "Tachi", "Yan", "Bijan", "Errol", "Skud", 
     "Superman", "Heino", "Charlotte", "Richter", "Arthur", "Airi", "Yena", 
     "Kil'Groth", "Ryoma", "Bolt Baron", "Amily", "Astrid", "Biron", "Billow", 
     "Roxie", "Wonder Woman", "Wiro", "Omen", "Rourke", "Veres", "Qi", "Dextra", "Allain"
   ],
-  "Pháp sư": [
+  "🔥Pháp sư": [
     "Mganga", "Veera", "Krixi", "Sephera", "Kahlii", "Ishar", "Bonnie", "Yue",
     "Raz", "Natalya", "Illumia", "Annette", "Điêu Thuyền", "Iggy", "Zata", 
     "Liliana", "Aleister", "Lauriel", "Jinna", "Preyta", "Bolt Baron", "Marja", 
     "Azzen'Ka", "Ignis", "Tulen", "D'Arcy", "Dirak", "Lorion", "Heino"
   ],
-  "Trợ thủ": [
+  "🛡️Trợ thủ": [
     "Toro", "Gildur", "Grakk", "Sephera", "Helen", "Ishar", "Krizzix", "Chaugnar", 
     "Annette", "Baldum", "Omega", "Dolia", "Arum", "Lumburr", "Alice", "Mina", 
     "Aya", "Rouie", "Xeniel", "TeeMee", "Cresht", "Zip", "Ming", "Taara", "Ormarr", 
     "Arduin", "Maloch", "Max", "Thane", "Biron", "Roxie", "Skud", "Wiro", "Y'bneth", 
     "Ata", "Dextra"
   ],
-  "Sát thủ": [
+  "⚔️Sát thủ": [
     "Bright", "Kaine", "Butterfly", "Volkath", "Raz", "Kriknak", "Quillen", "Aoi", 
     "Ngộ Không", "Zata", "Zuka", "Murad", "Liliana", "Zephys", "Airi", "Ryoma", 
     "Zill", "Astrid", "Billow", "Paine", "Keera", "Yan", "Enzo", "Nakroth", 
     "The Flash", "Qi", "Sinestrea"
   ],
-  "Xạ thủ": [
+  "🏹Xạ thủ": [
     "Yorn", "Valhein", "Tel'Annas", "Violet", "Teeri", "Slimz", "Laville", "Fennik", 
     "Elsu", "Stuart", "Capheny", "Erin", "Hayate", "Lindis", "Eland'orr", "Moren", 
     "Celica", "Wisp", "Thorne"
