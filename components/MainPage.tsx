a521c02c952eb2a95d1c291f2efa084b00a5c48e
tloidev1208 <thanh.loi@bs.io.vn>
2025-11-01 09:03:58 +0700

    Update MainPage.tsx

--- components/MainPage.tsx
+++ components/MainPage.tsx
@@ -222,7 +222,7 @@ export default function MainPage() {
       "Hưng (~THD~Bucky)",
       "Kiệt (PhởBEEF)",
       "Nam (Vinamilk)",
-      "Trường (ⒷⓁⓊⒺⓈ)",
+      "Phong",
     ]);
 
   const resetDateTime = () => {
